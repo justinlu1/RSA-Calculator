31e8aa2da8b053716a7dd4fcd76b8d6e8cd39a43
picklepower24 <68477910+picklepower24@users.noreply.github.com>
2022-01-04 17:29:06 -0500

    Building encrypt/decrypt
    Added functionality for encrypting/decrypting messages

--- RSACalc.cpp
+++ RSACalc.cpp
@@ -25,40 +25,6 @@ std::ostream& operator<<(std::ostream& os, const Key& k) {
     return os;
 }
 
-int menu() {
-    bool invalid = true;
-    int choice = 0;
-
-    while (invalid) {
-        std::cout << "Welcome to Justin's RSA Calculator!" << std::endl;
-        std::cout << "Select what you'd like to calculate:\n 1. Create Keys\n 2. Encrypt Message \n 3. Decrypt message" << std::endl;;
-        std::cin >> choice;
-        switch (choice) {
-            case 1: {
-                std::cout << "Create Keys:" << std::endl;
-                invalid = false;
-                break;
-            }
-            case 2: {
-                std::cout << "Encrypt Message" << std::endl;
-                invalid = false;
-                break;
-            }
-            case 3: {
-                std::cout << "Decrypt Message" << std::endl;
-                invalid = false;
-                break;
-            }
-            default: {
-                invalid = true;
-                std::cout << choice << " is an invalid choice. Please select from the three options below.\n" << std::endl;
-                break;
-            }
-        }
-    }
-    return choice;
-}
-
 // Determines if n is prime
 bool isPrime(int n) {
     int limit = ceil(sqrt(n));
@@ -74,6 +40,11 @@ bool isPrime(int n) {
 int gcd(int a, int b) {
    if (b == 0) return a;
    return gcd(b, a % b);
+}
+
+long long int flt(int base, int power, int primeModulo) {
+    int tempMod = power % (primeModulo - 1);
+    return pow(base, tempMod);
 }
 
 void createKeys() {
@@ -113,13 +84,11 @@ void createKeys() {
             }
         }
         if (invalidE == true) {
-            std::cout << "Invalid e value. Please choose a value from the list above.";
+            std::cout << "Invalid e value. Please choose a value from the list above. ";
         } else {
-            std::cout << "\nOkay, you chose e = " << e << std::endl;
+            std::cout << "\nOkay, you chose e = " << e << "." << std::endl;
         }
     }    
-
-    std::cout << "Now, we'll calculate a d value for your private key such that de = 1 (mod (p-1)(q-1) )." << std::endl << std::endl;
     while (invalidD) {
         if ((e * step) % ((p-1) * (q-1)) == 1) {
             invalidD = false;
@@ -127,7 +96,6 @@ void createKeys() {
             step++;
         }
     }
-    
 
     d = step;
     Key publicKey = Key(e, n);
@@ -139,11 +107,39 @@ void createKeys() {
 
 
 void encryptMessage() {
-    
+    int e, n, m, c, p, q;
+    std::cout << "Enter the e value of the public key: ";
+    std::cin >> e;
+    std::cout << "Enter the n value of the public key: ";
+    std::cin >> n;
+    std::cout << "Enter the message as an integer: ";
+    std::cin >> m;
+
+
+    // M = 25, e = 23, n = 143
+    // Expecting C = 38
+    for (int i = 2; i < n; i++) {
+        if ((n % i == 0) && isPrime(i)) {
+            p = i;
+            q = n / i; // prime factorizing n
+        }
+    }
+
+    long long int flt1 = flt(m, e, p);
+    long long int flt2 = flt(m, e, q);
+    std::cout << flt1 << ", " << flt2 << std::endl;
+
+    for (int i = 0; i < n; i++) {
+        if ((i % p == flt1 % p) && (i % q == flt2 % q)) {
+            c = i;
+        }
+    }
+
+    std::cout << "Your encrypted ciphertext is: " << c << std::endl;
 }
 
 void decryptMessage() {
-    int d, n, c;
+    int d, n, c, p, q, r;
     long long int remainder = 0;
     bool invalidR = true;
     std::cout << "Please enter the d value of the private key." << std::endl;
@@ -153,32 +149,90 @@ void decryptMessage() {
     std::cout << "Please enter the ciphertext as an integer." << std::endl;
     std::cin >> c;
     std::cout << "We want to solve the congruence R = " << c << "^" << d << "(mod " << n << ")." << std::endl;
-    remainder = pow(c, d);
-    remainder = remainder % n;
-    /*for (int i = 0; i < n; i++) {
-        if (i % n == remainder) {
-            invalidR = false;
-            remainder = i;
+
+    // Example: d = 27, n = 55, C = 47, expect M = 53
+    // solve M = C^d (mod n), or M = 47^27 (mod 55)
+    if (c > n) {
+        c = c % n;
+    }
+    for (int i = 2; i < n; i++) {
+        if ((n % i == 0) && isPrime(i)) {
+            p = i;
+            q = n / i; // prime factorizing n
         }
     }
-    if (invalidR == true) {
-        std::cout << "Something went wrong... One of your values might not be valid." << std::endl;
-    } else {*/
-        std::cout << "Your recovered message (R) is: " << remainder << std::endl;
-    //}
+    long long int flt1 = flt(c, d, p);
+    long long int flt2 = flt(c, d, q);
+
+    for (int i = 0; i < n; i++) {
+        if ((i % p == flt1 % p) && (i % q == flt2 % q)) {
+            r = i;
+        }
+    }
+
+    std::cout << "Your recovered message (R) is: " << r << std::endl;
+}
+
+int menu() {
+    bool invalid = true;
+    int choice = 0;
+
+    while (invalid) {
+        std::cout << "\n \n \nWelcome to Justin's RSA Calculator!" << std::endl;
+        std::cout << "Select what you'd like to calculate:\n 1. Create Keys\n 2. Encrypt Message \n 3. Decrypt message \n 4. Exit" << std::endl;;
+        std::cin >> choice;
+        switch (choice) {
+            case 1: {
+                std::cout << "Create Keys:" << std::endl;
+                invalid = false;
+                break;
+            }
+            case 2: {
+                std::cout << "Encrypt Message" << std::endl;
+                invalid = false;
+                break;
+            }
+            case 3: {
+                std::cout << "Decrypt Message" << std::endl;
+                invalid = false;
+                break;
+            }
+            case 4: {
+                std::cout << "Exiting..." << std::endl;
+                invalid = false;
+                break;
+            }
+            default: {
+                invalid = true;
+                std::cout << choice << " is an invalid choice. Please select from the three options below.\n" << std::endl;
+                break;
+            }
+        }
+    }
+    return choice;
 }
 
 int main() {
-    switch (menu()) {
-        case 1: {
-            createKeys();
-        }
-        case 2: {
-            encryptMessage();
-        }
-        case 3: {
-            decryptMessage();
+    bool loopMain = true;
+    while (loopMain) {
+        switch (menu()) {
+            case 1: {
+                createKeys();
+                break;
+            }
+            case 2: {
+                encryptMessage();
+                break;
+            }
+            case 3: {
+                decryptMessage();
+                break;
+            }
+            case 4: {
+                return 0;
+            }
         }
     }
+    
     return 0;
 }
